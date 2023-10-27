25d3731bf1ef52a7ba383f0a3deb7f8ddef1701a
hhermerschmidt <henning.hermerschmidt@gmail.com>
2023-10-27 12:34:10 +0000

    Add plot function

--- script/script.py
+++ script/script.py
@@ -1,19 +1,26 @@
 from pathlib import Path
 
 import pandas as pd
+import matplotlib.pyplot as plt
 
+FOLDER = Path(__file__).parent
 
+def plot(df: pd.DataFrame, save_path: Path = FOLDER):
+    fig = plt.figure(figsize=(10,8))
+    ax = fig.add_axes((0.05, 0.05, 0.9, 0.9))
+    
+    ax.plot(df["Index"].array, df["First Column"].array)
+    fig.savefig(save_path / "figure.png")
+    
 def read_data(file: Path):
-    df = pd.read_csv(file)
-    print(df)
-
-def plot():
-    pass
+    df = pd.read_csv(file, skipinitialspace=True)
+    return df
 
 def main(file: Path):
-   read_data(file) 
+   df = read_data(file)
+   plot(df)
     
 if __name__ == "__main__":
 
-    data_file = Path(__file__).parent / "data.csv"
+    data_file = FOLDER / "data.csv"
     main(data_file)
