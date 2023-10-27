710b93e075b43debb3b8d3c727c628a4d627b449
hhermerschmidt <henning.hermerschmidt@gmail.com>
2023-10-27 11:03:03 +0000

    Read datafile

--- script/script.py
+++ script/script.py
@@ -1,14 +1,19 @@
+from pathlib import Path
+
 import pandas as pd
 
-def main():
-    pass
 
-def read_data():
-    pass
+def read_data(file: Path):
+    df = pd.read_csv(file)
+    print(df)
 
 def plot():
     pass
 
+def main(file: Path):
+   read_data(file) 
+    
 if __name__ == "__main__":
 
-    print("Hello World!")
+    data_file = Path(__file__).parent / "data.csv"
+    main(data_file)
